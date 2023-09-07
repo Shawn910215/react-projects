3fbb81121d4b31dd5ce9e35dea8793a6ff2bf7b5
shang yang <yangshang910215@gmail.com>
2023-09-07 11:18:24 -0400

    handle the event in element

--- steps/src/App.js
+++ steps/src/App.js
@@ -5,23 +5,31 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 2;
+  const step = 1;
+  function handlePrevious() {}
+  function handleNext() {}
   return (
     <div className="steps">
       <div className="numbers">
         <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 1 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 1 ? "active" : ""}`}>3</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
       </div>
 
       <p className="message">
         Step {step}: {messages[step - 1]}
       </p>
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious} //do not add ()
+        >
           Previsou
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
