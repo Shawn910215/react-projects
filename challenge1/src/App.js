3cab749e2fe6a11cc7660c23a1230a9bc0b7c481
shang yang <yangshang910215@gmail.com>
2023-09-12 15:52:33 -0400

    finish the funny challenge

--- challenge1/src/App.js
+++ challenge1/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  // function handleStep() {}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div>
+        <button onClick={step > 0 ? () => setStep((s) => s - 1) : null}>
+          -
+        </button>
+        <span>{`Step:${step}`}</span>
+        <button onClick={(s) => setStep(step + 1)}>+</button>
+      </div>
+      <div>
+        <button
+          onClick={count - step >= 0 ? (s) => setCount(count - step) : null}
         >
-          Learn React
-        </a>
-      </header>
+          -
+        </button>
+        <span>{`Count:${count}`}</span>
+        <button onClick={(s) => setCount(count + step)}>+</button>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${count} days ago was`}
+        </span>
+        {date.toDateString()}
+      </p>
     </div>
   );
 }
