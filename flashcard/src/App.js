477755fca036ce34ef889f8a26d5169db03fe87f
shang yang <yangshang910215@gmail.com>
2023-09-19 22:09:10 -0400

    FlashCard finished

--- flashcard/src/App.js
+++ flashcard/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+const flashcards = [
+  { id: 1, question: "A", answer: "a" },
+  { id: 2, question: "B", answer: "b" },
+  { id: 3, question: "C", answer: "c" },
+];
 
-function App() {
+export default function App() {
+  const [selectedId, setSelectedId] = useState(null);
+  const flashHandle = function (id) {
+    setSelectedId(id !== selectedId ? id : null);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      {flashcards.map((item) => (
+        <p onClick={() => flashHandle(item.id)}>
+          {item.id === selectedId ? item.answer : item.question}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ))}
     </div>
   );
 }
-
-export default App;
