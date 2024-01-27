46ddb8415e33ff7beff73fb94441861449c93cc6
shang yang <yangshang910215@gmail.com>
2024-01-27 09:34:24 -0500

    fetch local API and conditional render Loader,Error,Start by the status property of state

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,9 +1,50 @@
-import DateCounter from "./DateCounter";
+import Main from "./Main";
+import Header from "./Header";
+import { useEffect, useReducer } from "react";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
+const initialState = {
+  questions: [],
+  //"loading", "error", "ready", "active", "finished"
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Unkown action");
+  }
+}
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { questions, status } = state;
+  const numQuestion = questions.length;
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "ready" && <StartScreen numQuestion={numQuestion} />}
+        {status === "error" && <Error />}
+      </Main>
     </div>
   );
 }
---
+++ react-quiz/src/StartScreen.js
@@ -0,0 +1,13 @@
+function StartScreen({ numQuestion }) {
+  return (
+    <div className="start">
+      <h2>Welcome to The React Quiz!</h2>
+      <h3>{numQuestion} questions to test your React mastery</h3>
+      <button type="" className="btn btn-ui">
+        Let's start
+      </button>
+    </div>
+  );
+}
+
+export default StartScreen;
