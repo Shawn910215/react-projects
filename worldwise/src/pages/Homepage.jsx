7fe668274023737d1942a220e71527e0e6c03300
shang yang <yangshang910215@gmail.com>
2024-01-30 16:47:23 -0500

    use <Link> to create NavBar component. Also use <NavLink> instead it to let the selected one has class="active", which allows to use css to show who was selected.

--- worldwise/src/pages/Homepage.jsx
+++ worldwise/src/pages/Homepage.jsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
 function Homepage() {
-  return <div>WorldWise</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>WorldWise</h1>
+      {/* use traditional anchor, it will reload whole page,which means send request to server. */}
+      {/* <a href="/pricing">Pricing</a> */}
+
+      {/* Use <Link> can build the navigate on client side */}
+      {/* <Link to="/pricing">Pricing</Link> */}
+    </div>
+  );
 }
 
 export default Homepage;
